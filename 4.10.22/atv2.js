621b8640f2c029285146ced033d86bcb70b27428
Matheus Santana <99267136+MatheusBigode@users.noreply.github.com>
2022-10-04 12:56:13 -0300

    Add files via upload

---
+++ 4.10.22/atv2.js
@@ -0,0 +1,15 @@
+function Verificar() {
+    var numero = Number(document.getElementById("numero").value)
+
+    if(numero == 0){
+        document.getElementById("res").innerHTML = "Seu número é 0!"
+    } else {
+        if(numero > 0) {
+        document.getElementById("res").innerHTML = "Seu número é natural!"}
+        else {
+        document.getElementById("res").innerHTML = "Seu número é negativo!"
+        }
+       
+    }
+}
+
